09d55f632956d2245f5275c88d66ebc59b2d3769
kkbt <0@ftls.xyz>
2023-03-07 12:42:36 +0800

    docs: 增加了一些文档说明

--- README.md
+++ README.md
@@ -1,22 +1,60 @@
+# Obsidian S3 存储的后端 API Golang 版本
+
+python 老版本 https://gitee.com/kkbt/obsidian-csapi 相关说明可能算是更详细一点。可以借鉴一些其中的配置说明，和实际效果。
+
+基于 Obsidian S3 存储的后端 API ,保存到 S3 存储的 Obsidian 库。支持列表
+
+一个简易前端（后有图）
+微信测试号 微信到Obsidian  
+支持简悦 SimpRead Webook  
+支持 fv悬浮球文字图片分享保存  
+静读天下 MoonReader 高亮标注 仿 ReadWise API  
+通用 http api  
+邮件发送登录链接
 
 
-前端后端交互
-
-前端完全不依赖后端，后端仅仅类似 Nginx 代理静态文件。Host 默认使用，但是不强制。如浏览器初始化默认存储当前域名和子域名。但是可能不会正常显示。
-
-后端定时更换 Token 
-打算实现一个邮件发送登录链接，从而实现前端登录。
+后端定时更换 Token 。实现了一个邮件发送登录链接，从而实现前端登录。
 
 两种 token 
 1. 全权限 token 包括增删改查 （有效期内可用，配置中写明邮件，发送到邮箱从而获取有效 token）
 2. 只能发送信息的 token (只要不改一直有效)
 
+## 部署
 
-## tests commands
+复制 config.examples.yaml 为 config.yaml 。部署时建议把项目文件夹内文件都复制过去。（至少包含 template , token 两个文件夹中，及其相关内容。 tem.txt 和 config.yaml 两个文件。
 
-```bash
-go test -v tools.go main_test.go 
-```
 
-现在这玩意算是能用，但是这个 token 系统还称不上好用。还是不如
+现在这玩意算是能用，但是这个 token 系统还称不上好用。
 
+## 展示
+
+后台发送的邮件
+
+![](docs/images/Snipaste_2023-03-07_11-36-48.png)
+
+点击进入的样子
+
+![](docs/images/Snipaste_2023-03-07_11-37-38.png)
+
+## 开发说明
+
+Go 语言开发
+
+入口文件  
+
+server.go 可按照需求，删减相关路径。
+mail.go 邮件相关代码
+s3.go 对 对象存储 操作相关的代码
+token.go Token 的生成，修改，验证代码
+wechat.go 微信公众号/测试号，相关代码
+handlers.go 其余路径的一些代码
+
+文件说明
+
+token/token1 存储 token1 全权限 token 
+token/token2 存储只发送 token2 用于一些只发送的其他第三方程序 api
+template/index.html 前端文件，一般不使用。但是也可以用。在配置文件中修改前端地址。邮件登录链接就会使用相关前端
+template/404.html 404 文档
+tem.txt 存储请求的 markdown 文本（因为 s3 sdk 文档很复杂，暂时用这方法）
+build.sh 将 go 打包成无链接的静态文件
+http/test.http vscode 插件 postcode 使用的，用于测试
--- server.go
+++ server.go
@@ -23,19 +23,19 @@ func main() {
 
 	http.Handle("/", http.FileServer(http.Dir("./template")))
 	http.HandleFunc("/404", BaseHandler)                                          // 404
-	http.HandleFunc("/token", VerifyToken1Handler)                                // Token 验证
+	http.HandleFunc("/token", VerifyToken1Handler)                                // Token 验证 测试使用
 	http.HandleFunc("/api/wechat", wechatmpfunc)                                  // wecheet 机器人 用于公众测试号
-	http.HandleFunc("/ob/today", ob_today)                                        // Obsidian Token1
-	http.HandleFunc("/ob/today/all", ob_today_all)                                // Obsidian Token1
-	http.HandleFunc("/ob/recent", get_3_day)                                      // Obsidian Token1
-	http.HandleFunc("/ob/moonreader", moodreader)                                 // Obsidian Token2
-	http.HandleFunc("/ob/fv", fvHandler)                                          // Obsidian Token2
-	http.HandleFunc("/time", greet)                                               // 打招呼 测试使用
-	http.Handle("/api/sendtoken2mail", limit(http.HandlerFunc(SendTokenHandler))) // 请求将 token发送到 email
+	http.HandleFunc("/ob/today", ob_today)                                        // Obsidian Token1 GET/POST 今日日记
+	http.HandleFunc("/ob/today/all", ob_today_all)                                // Obsidian Token1 POST 整片修改今日日记
+	http.HandleFunc("/ob/recent", get_3_day)                                      // Obsidian Token1 GET 近三天日记
+	http.HandleFunc("/ob/moonreader", moodreader)                                 // Obsidian Token2 POST 静读天下 api
+	http.HandleFunc("/ob/fv", fvHandler)                                          // Obsidian Token2 POST 安卓 FV 悬浮球 快捷存储 文字，图片
+	http.HandleFunc("/time", greet)                                               // 打招呼 测试使用 GET
+	http.Handle("/api/sendtoken2mail", limit(http.HandlerFunc(SendTokenHandler))) // 请求将 token发送到 email GET 请求
 	http.ListenAndServe(fmt.Sprintf("%s:%s", ConfigGetString("host"), ConfigGetString("port")), nil)
 }
 
-var limiter = rate.NewLimiter(0.00001, 1)
+var limiter = rate.NewLimiter(0.00001, 1) // 限制 token 发送到 email (0.01 ,1) 意思 100 秒，允许 1 次。
 
 // 短时间多次请求限制
 func limit(next http.Handler) http.Handler {
