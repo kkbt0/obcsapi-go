e3677ffb6074d18601c4fed6e6e422f295e8371d
kkbt <0@ftls.xyz>
2023-03-20 15:12:08 +0800

    refactor: if -> switch case

--- wechat.go
+++ wechat.go
@@ -23,31 +23,32 @@ func wechatmpfunc(w http.ResponseWriter, r *http.Request) {
 	r_str := "📩 已保存，<a href='https://note.ftls.xyz/#/ZK/202209050658'>点击查看今日笔记</a>"
 	client, err := get_client()
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
-	if mp.Request.MsgType == weixinmp.MsgTypeText { // 文字消息
-		append_memos_in_daily(client, mp.Request.Content) //
-		mp.ReplyTextMsg(w, r_str)
-	} else if mp.Request.MsgType == weixinmp.MsgTypeImage { // 图片消息
+	switch mp.Request.MsgType {
+	case weixinmp.MsgTypeText: // 文字消息
+		err = append_memos_in_daily(client, mp.Request.Content) //
+	case weixinmp.MsgTypeImage: // 图片消息
 		fileby, _ := downloader(mp.Request.PicUrl)
 		file_key := fmt.Sprintf("日志/附件/%s/%s.jpg", timeFmt("200601"), timeFmt("20060102150405"))
 		store(client, file_key, fileby)
 		// 前端会监测 ![https://..](..) 将 http:// 放到 后面 ![..](https://..)
 		// append_memos_in_daily(client, fmt.Sprintf("![%s](%s)", mp.Request.PicUrl, file_key))
-		append_memos_in_daily(client, fmt.Sprintf("![](%s)", file_key))
-		mp.ReplyTextMsg(w, r_str)
-	} else if mp.Request.MsgType == weixinmp.MsgTypeVoice { // 语言消息
-		append_memos_in_daily(client, fmt.Sprintf("语音: %s", mp.Request.Recognition))
-		mp.ReplyTextMsg(w, r_str)
-	} else if mp.Request.MsgType == weixinmp.MsgTypeLocation { // 位置消息
-		append_memos_in_daily(client, fmt.Sprintf("位置信息: 位置 %s <br>经纬度( %f , %f )", mp.Request.Label, mp.Request.LocationX, mp.Request.LocationY))
-		mp.ReplyTextMsg(w, r_str)
-	} else if mp.Request.MsgType == weixinmp.MsgTypeLink { // 链接消息
-		append_memos_in_daily(client, fmt.Sprintf("[%s](%s)<br>%s...", mp.Request.Title, mp.Request.Url, mp.Request.Description))
-		mp.ReplyTextMsg(w, r_str)
-	} else if mp.Request.MsgType == weixinmp.MsgTypeVideo {
-		mp.ReplyTextMsg(w, "不支持的视频消息")
-	} else {
-		mp.ReplyTextMsg(w, "未知消息")
+		err = append_memos_in_daily(client, fmt.Sprintf("![](%s)", file_key))
+	case weixinmp.MsgTypeVoice: // 语言消息
+		err = append_memos_in_daily(client, fmt.Sprintf("语音: %s", mp.Request.Recognition))
+	case weixinmp.MsgTypeLocation: // 位置消息
+		err = append_memos_in_daily(client, fmt.Sprintf("位置信息: 位置 %s <br>经纬度( %f , %f )", mp.Request.Label, mp.Request.LocationX, mp.Request.LocationY))
+	case weixinmp.MsgTypeLink: // 链接消息
+		err = append_memos_in_daily(client, fmt.Sprintf("[%s](%s)<br>%s...", mp.Request.Title, mp.Request.Url, mp.Request.Description))
+	case weixinmp.MsgTypeVideo:
+		r_str = "不支持的视频消息"
+	default:
+		r_str = "未知消息"
 	}
+	if err != nil {
+		log.Println(err)
+		r_str = "Error"
+	}
+	mp.ReplyTextMsg(w, r_str)
 }
