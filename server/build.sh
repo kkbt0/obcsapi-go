af79822d115368aee822dfc2f562141ceb872b8d
kkbt <0@ftls.xyz>
2023-04-09 11:32:00 +0800

    sh: 构建脚本

--- server/build.sh
+++ server/build.sh
@@ -1,5 +1,18 @@
-rm -rf server && rm -rf server.zip
+#!/bin/bash
+rm -rf output
 go build -o server  -ldflags '-linkmode "external" -extldflags "-static"' .
 # CGO_ENABLED=1 GOOS=linux GOARCH=amd64 CC=x86_64-linux-musl-gcc CGO_LDFLAGS=“-static” go build -o server 
-zip server.zip server
-ldd server 
+# zip server.zip server
+ldd server
+mkdir output
+cp config.example.yaml output/config.yaml
+cp server output/
+cp -R static/ output/
+cp -R templates/ output/
+cd output 
+echo "Hello" > tem.txt
+mkdir webdav
+cd webdav
+mkdir images
+cd ..
+zip -r -m obcsapi.$1.zip * 
