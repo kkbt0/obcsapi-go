5b08b725db0a66b0455286780d107f62854f4ff6
kkbt <0@ftls.xyz>
2023-05-20 12:47:43 +0800

    feat:  fix grep err

--- server/sh/lastinput.sh
+++ server/sh/lastinput.sh
@@ -8,4 +8,6 @@ date=$(date +%Y%m%d)
 filename="dialogues.${date}.log"
 
 # 获取最后一行以I:开头的行
-grep "^I:" ./${filename} | tail -n 1
+# grep "^I:" ./${filename} | tail -n 1
+# 获取最后一行以I:开头的行到文件结束
+tail -n +$(grep -n "^I:" $filename | tail -1 | cut -d: -f1) $filename
