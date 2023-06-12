f96f0b4399533ca5838c0b77c63694ad79625f53
kkbt <0@ftls.xyz>
2023-06-12 19:04:37 +0800

    feat: talk markdown & images upload

--- obcsapi-web/src/views/TalkView.vue
+++ obcsapi-web/src/views/TalkView.vue
@@ -5,10 +5,15 @@ import { ObcsapiTalk } from "@/api/obcsapi";
 import { TalkStore } from "@/stores/talk";
 import { LocalSetting } from "@/stores/setting"
 
+import marked from "marked";
+import MemosUpload from "@/components/obcsapi/MemosUpload.vue";
+
 const messages = TalkStore().messages;
 const newMessage = ref('');
 const scrollbarRef: Ref<any> = ref(null);
 const contentRef: Ref<any> = ref(null);
+const talkInputRef = ref();
+const showUpload = ref(false);
 
 function sendMessage() {
   if (newMessage.value == '') {
@@ -17,6 +22,7 @@ function sendMessage() {
   }
   messages.push("I: " + newMessage.value);
   ObcsapiTalk(newMessage.value).then(text => {
+    showUpload.value = false;
     messages.push("O: " + text);
     scrollToBottom()
   }).catch(e => {
@@ -34,6 +40,21 @@ const scrollToBottom = () => {
   scrollbarRef.value.scrollTo(0, scrollY);
 };
 
+function addTags() {
+  newMessage.value += '#'
+  talkInputRef.value?.focus();
+}
+
+function imgUrlDeal(text: string) {
+  newMessage.value += `\n${text}\n`;
+}
+
+function markdown(text: string) :string {
+  return marked(text || '', {
+        breaks: true
+    })
+}
+
 </script>
 
 <template>
@@ -41,14 +62,19 @@ const scrollToBottom = () => {
     <div>Talk</div>
     <div class="chat-input">
       <n-mention type="textarea" :autosize="{ minRows: 2 }" :options="LocalSetting().mention" v-model:value="newMessage"
-        placeholder=":~$" :prefix="['#']" />
-      <n-button @click="sendMessage">输入</n-button>
+        placeholder=":~$" :prefix="['#']" ref="talkInputRef"/>
     </div>
+    <n-space justify="space-between">
+      <n-button quaternary type="info" @click="showUpload = !showUpload">📌</n-button>
+      <n-button quaternary type="info" @click="addTags">🏷️</n-button>
+      <n-button quaternary type="info" @click="sendMessage">🚀</n-button>
+    </n-space>
+    <MemosUpload v-if="showUpload" @upload-callback="imgUrlDeal" />
     <n-scrollbar style="max-height: 75vh" ref="scrollbarRef">
       <div class="chat-messages" ref="contentRef">
         <div v-for="(message, index) in messages.slice().reverse()" :key="index">
-          <div v-if="message.substring(0, 2) == 'I:'" class="message1" v-html="message"></div>
-          <div v-else class="message2" v-html="message"></div>
+          <div v-if="message.substring(0, 2) == 'I:'" class="message1" v-html="markdown(message)"></div>
+          <div v-else class="message2" v-html="markdown(message)"></div>
         </div>
       </div>
     </n-scrollbar>
@@ -57,12 +83,17 @@ const scrollToBottom = () => {
   
 
   
-<style>
+<style scoped>
 .chat-messages {
   overflow-y: auto;
   margin-bottom: 10px;
   overflow: hidden;
   /* 添加此样式以隐藏溢出内容 */
+}
+
+.chat-messages :deep() img {
+  max-width: 100%;
+  display: block;
 }
 
 .message1 {
