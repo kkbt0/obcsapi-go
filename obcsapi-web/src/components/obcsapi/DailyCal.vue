9167da97d5214d52fe8a6b1956bf1b86b6fd2a36
kkbt <0@ftls.xyz>
2023-06-15 13:39:22 +0800

    fix: sum err

--- obcsapi-web/src/components/obcsapi/DailyCal.vue
+++ obcsapi-web/src/components/obcsapi/DailyCal.vue
@@ -16,15 +16,19 @@ function handleUpdateValue(
 }
 
 function GetFileKey(year: number, month: number, date: number): string {
+    return LocalSetting().localSetting.CalObDailyFir + GetFileKeyPart2(year, month, date)
+}
+
+function GetFileKeyPart2(year: number, month: number, date: number): string {
     const f_month = month.toString().padStart(2, '0');
     const f_date = date.toString().padStart(2, '0');
     let template = LocalSetting().localSetting.CalObDaily;
     let reslut = template.replace("2006", "T_Y").replace("01", "T_M").replace("02", "T_D")
-    return LocalSetting().localSetting.CalObDailyFir + reslut.replace("T_Y", year.toString()).replace("T_M", f_month.toString()).replace("T_D", f_date.toString())
+    return  reslut.replace("T_Y", year.toString()).replace("T_M", f_month.toString()).replace("T_D", f_date.toString())
 }
 
 function GetMemosFromDate(year: number, month: number, date: number): string {
-    let str = memos.memosMap.get(GetFileKey(year, month, date))
+    let str = memos.memosMap.get(GetFileKeyPart2(year, month, date))
     if (str != undefined) {
         return str.md_text.length
     } else {
