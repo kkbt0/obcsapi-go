e9f904904a009274441f1195d4cb1c8c342b1d2b
kkbt <0@ftls.xyz>
2023-06-21 17:37:47 +0800

    build bash

---
+++ step2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd server
+version="4.2.5"
+docker build -t kkbt/obcsapi:v$version .
+docker build -t kkbt/obcsapi:latest .
+docker save -o ob4.2.tar kkbt/obcsapi:v$version && gzip ob4.2.tar
+bash build.sh $version
