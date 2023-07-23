5a60d59cc946eef69051aff060602f788b442dd2
kkbt <0@ftls.xyz>
2023-07-23 12:18:43 +0800

    feat: more arch build support

--- step2.sh
+++ step2.sh
@@ -4,6 +4,7 @@ go mod tidy
 go mod vendor
 version="4.2.6"
 docker build -t kkbt/obcsapi:v$version .
-docker build -t kkbt/obcsapi:latest .
+docker build -t kkbt/obcsapi:latest ..
+# docker buildx build -t kkbt/obcsapi:latest --platform=linux/arm,linux/arm64,linux/amd64 .
 docker save -o ob4.2.tar kkbt/obcsapi:v$version && gzip ob4.2.tar
 bash build.sh $version
