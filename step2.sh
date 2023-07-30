9679ca8b8772332928f2994f9424795cf8e936e9
kkbt <0@ftls.xyz>
2023-07-30 13:23:43 +0800

    pref

--- step2.sh
+++ step2.sh
@@ -3,8 +3,10 @@ cd server
 go mod tidy 
 go mod vendor
 version="4.2.7"
-docker build -t kkbt/obcsapi:v$version .
-docker build -t kkbt/obcsapi:latest ..
-# docker buildx build -t kkbt/obcsapi:latest --platform=linux/arm,linux/arm64,linux/amd64 .
+docker buildx build -t kkbt/obcsapi:v$version --platform=linux/arm,linux/arm64,linux/amd64 . --push
+docker buildx build -t kkbt/obcsapi:latest --platform=linux/arm,linux/arm64,linux/amd64 . --push
+# docker build -t kkbt/obcsapi:v$version .
+# docker build -t kkbt/obcsapi:latest ..
+# docker buildx build -t kkbt/obcsapi:latest --platform=linux/arm,linux/arm64,linux/amd64 . --push
 docker save -o ob4.2.tar kkbt/obcsapi:v$version && gzip ob4.2.tar
 bash build.sh $version
