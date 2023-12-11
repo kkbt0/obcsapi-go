204a60323d438ff6e0c06b3d763a3bf8dd303f4b
kkbt <0@ftls.xyz>
2023-12-11 17:40:43 +0800

    to: vuepress

--- obcsapi-docs/docs/.vuepress/config.js
+++ obcsapi-docs/docs/.vuepress/config.js
@@ -1,85 +1,48 @@
-import { defineUserConfig } from 'vuepress'
-import { defaultTheme } from 'vuepress'
-
-export default defineUserConfig({
+module.exports = {
   lang: 'zh-CN',
   title: 'Obsidian Cloud Storage API 文档',
   description: 'ObcsapiGo 文档',
+  host: '0.0.0.0',
+  port: 1313,
   markdown:{
     headers: {
       level: [1,2, 3, 4],
     }
   },
   sidebarDepth: 5,
-  theme: defaultTheme({
+  themeConfig: {
     sidebar: [
       {
-        text: 'Go版本',
-        link: '/md/go-version/1-概述.md',
+        title: 'Go版本',   // 必要的
+        collapsable: false, // 可选的, 默认值是 true,
+        sidebarDepth: 10,    // 可选的, 默认值是 1
         children: [
-          {
-            text: '1. 概述',
-            link: '/md/go-version/1-概述.md',
-          },
-          {
-            text: '2. 运行与部署',
-            link: '/md/go-version/2-运行与部署.md',
-          },
-          {
-           text: '3. 配置说明',
-           link: '/md/go-version/3-配置说明.md', 
-          },{
-            text: '4. 功能使用',
-            link: '/md/go-version/4-功能使用.md',
-          },{
-            text: '5. 图床说明',
-            link: '/md/go-version/5-图床说明.md',
-          },{
-            text: '6. 通用接口',
-            link: '/md/go-version/6-通用接口.md',
-          },{
-            text: '7. 前端说明',
-            link: '/md/go-version/7-前端说明.md',
-          },{
-            text: '8. 指令模式',
-            link: '/md/go-version/8-指令模式.md',
-          },{
-            text: '9. 自定义脚本',
-            link: '/md/go-version/9-自定义脚本.md',
-          },{
-            text: '10. 软件联动',
-            link: '/md/go-version/10-软件联动.md',
-          },{
-            text: '97. 缓存说明',
-            link: '/md/go-version/97-缓存说明.md',
-          },{
-            text: '98. 开发说明',
-            link: '/md/go-version/98-开发说明.md',
-          },{
-            text: '99. 更新记录',
-            link: '/md/go-version/99-更新记录.md',
-          }
+          ['/md/go-version/1-概述.md', '1. 概述'],
+          ['/md/go-version/2-运行与部署.md','2. 运行与部署'],
+          ['/md/go-version/3-配置说明.md','3. 配置说明'],
+          ['/md/go-version/4-功能使用.md','4. 功能使用'],
+          ['/md/go-version/5-图床说明.md','5. 图床说明'],
+          ['/md/go-version/6-通用接口.md','6. 通用接口'],
+          ['/md/go-version/7-前端说明.md','7. 前端说明'],
+          ['/md/go-version/8-指令模式.md','8. 指令模式'],
+          ['/md/go-version/9-自定义脚本.md','9. 自定义脚本'],
+          ['/md/go-version/10-软件联动.md','10. 软件联动'],
+          ['/md/go-version/97-缓存说明.md','97. 缓存说明'],
+          ['/md/go-version/98-开发说明.md','98. 开发说明'],
+          ['/md/go-version/99-更新记录.md','99. 更新记录']
         ]
-    },
-    {
-      text: 'Python版本',
-      link: '/md/python-version.md',
-    },
-    {
-      text: 'FAQ',
-      link: '/md/faq.md',
-    }
+      },
   ],
-    navbar: [
-        {
-            text: '首页',
-            link: '/',
-        },
-        {
-            text: 'Go 版本源码',
-            link: 'https://gitee.com/kkbt/obcsapi-go',
-        }
-    ]
-  }),
-})
+  navbar: [
+    {
+        text: '首页',
+        link: '/',
+    },
+    {
+        text: 'Go 版本源码',
+        link: 'https://gitee.com/kkbt/obcsapi-go',
+    }
+]
+  },
+}
 
