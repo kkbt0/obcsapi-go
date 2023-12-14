01b1f302724fce8a92489ed39e8f95d4afdf6233
恐咖兵糖 <0@ftls.xyz>
2023-12-14 13:42:57 +0000

    update obcsapi-docs/docs/.vuepress/theme.ts.
    Signed-off-by: 恐咖兵糖 <0@ftls.xyz>

--- obcsapi-docs/docs/.vuepress/theme.ts
+++ obcsapi-docs/docs/.vuepress/theme.ts
@@ -39,7 +39,7 @@ export default hopeTheme({
       provider: "Giscus",
       repo: "kkbt0/giscus",
       repoId: "R_kgDOIqRAow",
-      category: "Obcsapi",
+      category: "Announcements",
       categoryId: "DIC_kwDOIqRAo84CTNxi",
     },
 
